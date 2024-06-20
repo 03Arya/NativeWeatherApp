0ac52b07bced9453dac6ec3bd41854af31f0214a
03Arya <142574149+03Arya@users.noreply.github.com>
2024-06-20 10:31:47 +0200

    updated the app with more features and styling

--- src/app/second.tsx
+++ src/app/second.tsx
@@ -1,6 +1,8 @@
 import { Link } from "expo-router";
+import { useEffect, useState } from 'react';
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
+import Geolocation from '@react-native-community/geolocation';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
@@ -12,34 +14,28 @@ export default function Page() {
 }
 
 function Content() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
   return (
     <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
-            </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
+      <View className="">
+          <View className="flex flex-col items-center gap-4 text-center pt-3">
+          <Text>Latitude: {location?.latitude}</Text>
+          <Text>Longitude: {location?.longitude}</Text>
+          <TextInput className="bg-slate-700 w-screen max-w-96 text-white py-1 px-5 rounded-full" placeholder="Enter name of city" placeholderTextColor="white"></TextInput> 
             </View>
           </View>
         </View>
-      </View>
-    </View>
   );
 }
 

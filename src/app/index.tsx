1b5621447e6501e6b4d69633e2750f01d80024b9
03Arya <142574149+03Arya@users.noreply.github.com>
2024-06-20 13:52:14 +0200

    updated more content and styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,7 @@ import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { faArrowRight} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export default function Page() {
   return (
--- src/app/second.tsx
+++ src/app/second.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import React from "react";
 import { Text, View, TextInput } from "react-native";
 import * as Location from 'expo-location';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 
 
@@ -45,7 +47,9 @@ function Content() {
       <View className="">
         <View className="flex flex-col items-center gap-4 text-center pt-3">
           <Text>{text}</Text>
-          <TextInput className="bg-slate-700 w-screen max-w-96 text-white py-1 px-5 rounded-full" placeholder="Enter name of city" placeholderTextColor="white"></TextInput> 
+          <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+
+          <TextInput className="bg-slate-700 w-screen max-w-96 text-white py-1 px-5 rounded-full" placeholder="Enter name of city" placeholderTextColor="white"></TextInput>
         </View>
       </View>
     </View>
